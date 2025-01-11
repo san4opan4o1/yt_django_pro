39aa1be63740767f253e9b953208a40ad221d174
san4opan4o1 <eftenialex@gmail.com>
2025-01-11 16:43:22 +0200

    filters Humanize

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 #from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    #http://127.0.0.1:8000/hello/
     path("myapp/", include("myapp.urls", namespace="myapp"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
